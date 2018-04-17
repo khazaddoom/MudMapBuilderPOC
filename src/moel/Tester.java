bf4f1e756f52d3edce09b298e80fcef2e3102ee3
khazaddoom <31121986g@gmail.com>
2018-04-17 19:24:42 +0530

    This is a commit from my ACC Laptop

---
+++ src/moel/Tester.java
@@ -0,0 +1,32 @@
+package moel;
+
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Tester extends JPanel {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		
+		JFrame f = new JFrame();
+	      f.setSize(400, 400);
+	      f.add(new Tester());
+	      f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	      f.setVisible(true);
+		
+	}
+	
+	public void paint(Graphics g) {
+	      g.fillRect (5, 15, 50, 75);
+	      
+	      
+	      
+	   }
+
+}
